e78cf947811cbe64071c5ade6e54e598416d79de
araful39 <rajuslam39@gmail.com>
2025-05-31 01:08:54 +0000

    how to use array in jsx file

--- src/home.jsx
+++ src/home.jsx
@@ -1,4 +1,18 @@
 function Home() {
-  return <h2>🏠 Welcome to the Home Page</h2>;
+let home="Home";
+let page="Page";
+
+let name="Md Araful Islam";
+
+const cars = ["Saab", "Volvo", "BMW"];
+  return <>
+     
+
+     <h1>
+Arra is {cars.map((e,i)=>(<span key={i}>{e},</span>))}
+     </h1>
+  <h2>🏠 My Name is {name}</h2>
+  <h2>🏠 Welcome to the {home} {page}</h2>
+  </>
 }
 export default Home;
