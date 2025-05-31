916e8e80dbfdcfd0a79f928f9539f71fd2cd2a35
araful39 <rajuslam39@gmail.com>
2025-05-31 00:42:32 +0000

    routes use

---
+++ src/about.jsx
@@ -0,0 +1,4 @@
+function About() {
+  return <h2>ℹ️ About Us Page</h2>;
+}
+export default About;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,34 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <React.StrictMode>
     <App />
-  </StrictMode>,
-)
+  </React.StrictMode>
+);
+
+
+// createRoot(document.getElementById('root')).render(
+//   // <StrictMode>
+//     <App />
+//   // </StrictMode>,
+// )
+
+
+// createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+// export default function addabc(a,b){
+//   return a+b;
+// }
+
+
+
+// export const add =(a,b)=>{
+//   return a+b;
+// } 
